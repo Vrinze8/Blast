9fb2bb2c585f6b67c8fb9c7171979a7e86b78c07
Mercie Victoria <victoriamercy50@gmail.com>
2019-01-30 05:44:56 +0300

    Pass films on home view

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Film;
 use Illuminate\Http\Request;
 
 class FilmController extends Controller
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Film;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,10 +12,10 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index(Request $request)
+    public function index(Request $request, Film $film)
     {
         // $request->user()->authorizeRoles(['employee', 'manager']);
 
-        return view('home');
+        return view('home', ['films' => $film->all()]);
     }
 }
